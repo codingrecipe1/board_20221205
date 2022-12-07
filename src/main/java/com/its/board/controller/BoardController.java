08b7010577e76f2706507c46e9591da61d7b761c
codingrecipe1 <codingrecipe1@gmail.com>
2022-12-07 11:20:20 +0900

    09_axios 글수정 기능

--- src/main/java/com/its/board/controller/BoardController.java
+++ src/main/java/com/its/board/controller/BoardController.java
@@ -4,6 +4,8 @@ import com.its.board.dto.BoardDTO;
 import com.its.board.service.BoardService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -56,6 +58,12 @@ public class BoardController {
         BoardDTO boardDTO1 = boardService.findById(boardDTO.getId());
         model.addAttribute("board", boardDTO1);
         return "boardPages/boardDetail";
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody BoardDTO boardDTO) {
+        boardService.update(boardDTO);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
